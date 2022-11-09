b189582cdc8078c81eedce3b60cf5f8aa805f550
Władysław <89138444+vlzher@users.noreply.github.com>
2022-11-09 20:28:25 +0100

    cm6

--- src/assets/txts.js
+++ src/assets/txts.js
@@ -2,8 +2,7 @@ export const aboutMe = "\"        Lorem ipsum dolor sit amet, consectetur adipis
     " tempor incididunt ut labore et dolore magna aliqua." +
     " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi" +
     " ut aliquip ex ea commodo velit esse cillum dolore eu fugiat nulla pariatur." +
-    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia" +
-    " deserunt mollit anim id est laborum.\"";
+    " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia";
 
 export const languages = [
     {txt:"- Russian",level:"N"},
